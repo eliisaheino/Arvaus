0afe80d6346ad7c9d0d450cd12a7c649dbc6b86e
Your Name <eliisa.heino@myy.haaga-helia.fi>
2023-01-19 14:28:06 +0200

    tehtävä valmis 19.1.2023

--- App.js
+++ App.js
@@ -1,10 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { TextInput } from 'react-native';
+import { Alert } from 'react-native';
 
 export default function App() {
+
+    //Annettu numero tallennetaan number-tilamuuttujaan
+    //viesti-tilamuuttujaan asetetaan vastaus ja näytetään se käyttöliittymässä
+    const [number, setNumber] = useState ('');
+    const [viesti, setViesti] = useState ('');
+
+    //Arvausten lukumäärät
+    var guess = 1;
+   
+
+     //funktio suorittaa numeron arpomisen ja tarkistuksen
+     const pressedGuess = () => {
+
+      //Arvotaan satunnainen numero väliltä 1-100
+      const random = Math.floor(Math.random() * 10) + 1;
+      parseInt(number);
+
+      if(number == random){
+        Alert.alert('You guessed the number in:' + guess + 'guesses');
+        
+      }
+      else if(number > random){
+        guess++;
+        setViesti('Your guess is too high');
+
+      }else {
+        guess++;
+        setViesti('Your guess is too low')
+      
+      }
+     
+     }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> Guess a random number between 1-100! </Text>
+      <Text> {viesti}</Text>
+
+      {/*Syöttökenttä numeron arvaamiseen */}
+      {/*onChange antaa muuttujalle sisällön */}
+      <TextInput 
+          style={styles.input} 
+          onChangeText= {number => setNumber(number)}
+          value={number}
+          keyboardType="numeric"/>
+
+       {/*Painike, joka arpoo numeron */}   
+      <Button onPress={pressedGuess} title= "Make a guess" />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +65,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container2: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+  input : {
+    width:200  , 
+    borderColor: 'black', 
+    borderWidth: 1
+  },
+  text:{
+    fontSize: 20,
+    
+  }
 });
